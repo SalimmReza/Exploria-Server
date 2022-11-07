047096cdabb65d88dd6ec7ce8d025cb66ea2a427
SalimmReza <salimnuaa@gmail.com>
2022-11-07 23:32:43 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,10 +10,28 @@ const port = process.env.PORT || 5000;
 // assignment11-service-review
 // 6doGxRqcLhEWFxL3
 
+//serviceReview
+//services
+
 //middle ware
 
 app.use(cors());
 app.use(express.json())
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9snwll.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send("service review running")
